89b6de3803c91ab3782d0d713f52d1d90eff0bd3
mkotsollaris <mkotsollari@gmail.com>
2019-02-05 17:58:44 -0800

    added SIGNATURE_ALGORITHM and PRIVATE_KEY_FILE_PATH in dot Env

--- app/app.js
+++ app/app.js
@@ -1,58 +1,86 @@
-var express = require('express');
+var express = require('express')
 const request = require('request')
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
+const crypto = require('crypto')
+const fs = require('fs')
 require('dotenv').config()
-var app = express();
+var app = express()
 
 const headers = {
   "Authorization": "Basic " + new Buffer(process.env.TRULIOO_USERNAME + ':' + process.env.TRULIOO_PASSWORD).toString("base64"),
   "Content-Type": "application/json",
   "User-Agent": "trulioo-proxy/1.0.0.0"
-};
+}
 
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }))
 app.use(function (_, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
 app.get('/api/countryCodes', (_, res) => {
   request({ method: 'GET', url: process.env.TRULIOO_BASE_URL + '/configuration/v1/countrycodes/Identity%20Verification', headers: headers },
     (error, _, body) => {
       if (error) {
-        throw new Error(error);
+        throw new Error(error)
       }
-      res.setHeader('Content-Type', 'application/json');
-      res.send({ response: JSON.parse(body) });
-    });
-});
+
+      const signature = getSignatureByInput(body)
+
+      res.setHeader('Content-Type', 'application/json')
+      res.send({
+        response: body,
+        signature
+      })
+    })
+})
 
 app.get('/api/getFields/:countryCode', (req, res) => {
   request({ method: 'GET', url: process.env.TRULIOO_BASE_URL + '/configuration/v1/fields/Identity%20Verification/' + req.params.countryCode, headers: headers },
     (error, _, body) => {
       if (error) {
-        throw new Error(error);
+        throw new Error(error)
       }
-      res.setHeader('Content-Type', 'application/json');
-      res.send({ response: body });
-    });
-});
+
+      const signature = getSignatureByInput(body)
+
+      res.setHeader('Content-Type', 'application/json')
+      res.send({
+        response: body,
+        signature
+      })
+    })
+})
 
 app.post('/api/verify', (req, res) => {
   request({
-    method: 'POST',
-    url: `${process.env.TRULIOO_BASE_URL}/verifications/v1/verify`,
-    body: req.body,
-    headers: headers,
-    json: true
-  }, (error, _, body) => {
-    if (error) {
-      throw new Error(error);
-    }
-    res.setHeader('Content-Type', 'application/json');
-    res.send({ response: JSON.stringify(body) });
-  });
-});
+    method: 'POST', url: `${process.env.TRULIOO_BASE_URL}/verifications/v1/verify`, body: req.body, headers: headers, json: true
+  },
+    (error, _, body) => {
+      if (error) {
+        throw new Error(error)
+      }
+      const signature = getSignatureByInput(body)
 
-module.exports = app;
+      res.setHeader('Content-Type', 'application/json')
+      res.send({
+        response: body,
+        signature
+      })
+    })
+})
+
+const getSignatureByInput = (input) => {
+  const stringifiedInput = JSON.stringify(input)
+
+  let privatePem = fs.readFileSync(process.env.PRIVATE_KEY_FILE_PATH)
+  let key = privatePem.toString('ascii')
+  let sign = crypto.createSign(process.env.SIGNATURE_ALGORITHM)//process.env.SIGNATURE_ALGORITHM
+  sign.update(stringifiedInput)
+  let signature = sign.sign(key, 'hex')
+
+  return signature
+}
+
+module.exports = app
